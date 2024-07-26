c9d4a9d749625aa8afc4eae288def4e281ff942e
Yucen Xie <yucenxie5@gmail.com>
2024-07-26 13:11:25 -0400

    finished ch2 ex2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
@@ -8,6 +9,21 @@ const port = 3000;
 app.get('/', (req, res) => {
     res.send("Hello, World!");
 });
+
+// get requests to localhost:3000/video
+app.get("/video", async (req, res) => {
+    const vidPath = "./videos/intro 2021 - near end.mp4"; 
+    const stats = await fs.promises.stat(vidPath);
+
+    // write the response head, which includes video format and size
+    res.writeHead(200, {
+        "content-length": stats.size,
+        "content-type": "video/mp4"
+    });
+
+    // open a stream to read the vid, and pipe the read data to our response
+    fs.createReadStream(vidPath).pipe(res);
+})
 
 // start server?
 app.listen(port);
